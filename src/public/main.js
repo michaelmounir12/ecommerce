b3655eb20627e9008e41958939deb50013694dd4
michael <michaelmounirmnb@gmail.com>
2023-05-13 02:44:47 +0300

    newFile

--- src/public/main.js
+++ src/public/main.js
@@ -25,6 +25,79 @@ fetch('/dash.html')
   .then(html => {
     // Use the HTML content here
       mainContent.innerHTML = html;
+      const numofsales = document.querySelector("#sales");
+const earning = document.querySelector("#earn");
+const tbody = document.querySelector("#bod");
+const table = document.querySelector("#tab");
+
+
+
+function createSale(order)
+{
+
+     const tr = document.createElement("tr")
+     const name = document.createElement("td")
+     const price = document.createElement("td")
+     const payStatus = document.createElement("td")
+     const delStatus = document.createElement("td")
+     const span = document.createElement("span")
+
+     name.textContent = order.name;
+     price.textContent = `${order.price} EGP`;
+     payStatus.textContent = order.payStatus;
+     span.classList.add("status",order.delStatus)
+     span.textContent  = order.delStatus;
+     delStatus.appendChild(span);
+
+     tr.appendChild(name)
+     tr.appendChild(price)
+     tr.appendChild(payStatus)
+     tr.appendChild(delStatus)
+
+     tbody.appendChild(tr);
+
+}
+
+function createCustomer(customer)
+{
+
+const tr = document.createElement("tr")
+const td = document.createElement("td")
+const h4 = document.createElement("h4")
+h4.innerHTML = `${customer.name} <br> <span>${customer.country}</span>`
+td.appendChild(h4)
+tr.appendChild(td)
+table.appendChild(tr)
+
+       
+}  
+
+console.log(numofsales,earning,table,table)
+fetch("https://ecommerce-new.onrender.com/dash")
+.then(response => {
+  if (response.ok) {
+    return response.json(); // or response.text() for plain text response
+  }
+  throw new Error('Network response was not ok.');
+})
+.then(data => {
+  console.log(data)
+  numofsales.textContent = data.numofsales;
+  earning.textContent =`${data.earning} EGP` ;
+  data.customers.forEach((ele)=>
+  {
+     createCustomer(ele);
+  })
+  data.products.forEach((ele)=>
+  {
+     createSale(ele);
+  })
+
+
+})
+.catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
+});
   })
   .catch(error => {
     console.error('There was a problem fetching the HTML:', error);
@@ -69,6 +142,79 @@ Dash.addEventListener("click",()=>
   .then(html => {
     // Use the HTML content here
       mainContent.innerHTML = html;
+      const numofsales = document.querySelector("#sales");
+const earning = document.querySelector("#earn");
+const tbody = document.querySelector("#bod");
+const table = document.querySelector("#tab");
+
+
+
+function createSale(order)
+{
+
+     const tr = document.createElement("tr")
+     const name = document.createElement("td")
+     const price = document.createElement("td")
+     const payStatus = document.createElement("td")
+     const delStatus = document.createElement("td")
+     const span = document.createElement("span")
+
+     name.textContent = order.name;
+     price.textContent = `${order.price} EGP`;
+     payStatus.textContent = order.payStatus;
+     span.classList.add("status",order.delStatus)
+     span.textContent  = order.delStatus;
+     delStatus.appendChild(span);
+
+     tr.appendChild(name)
+     tr.appendChild(price)
+     tr.appendChild(payStatus)
+     tr.appendChild(delStatus)
+
+     tbody.appendChild(tr);
+
+}
+
+function createCustomer(customer)
+{
+
+const tr = document.createElement("tr")
+const td = document.createElement("td")
+const h4 = document.createElement("h4")
+h4.innerHTML = `${customer.name} <br> <span>${customer.country}</span>`
+td.appendChild(h4)
+tr.appendChild(td)
+table.appendChild(tr)
+
+       
+}  
+
+console.log(numofsales,earning,table,table)
+fetch("https://ecommerce-new.onrender.com/dash")
+.then(response => {
+  if (response.ok) {
+    return response.json(); // or response.text() for plain text response
+  }
+  throw new Error('Network response was not ok.');
+})
+.then(data => {
+  console.log(data)
+  numofsales.textContent = data.numofsales;
+  earning.textContent =`${data.earning} EGP` ;
+  data.customers.forEach((ele)=>
+  {
+     createCustomer(ele);
+  })
+  data.products.forEach((ele)=>
+  {
+     createSale(ele);
+  })
+
+
+})
+.catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
+});
   })
   .catch(error => {
     console.error('There was a problem fetching the HTML:', error);
@@ -142,76 +288,3 @@ signout.addEventListener("click",async()=>
 })
 
 
-const numofsales = document.querySelector("#sales");
-const earning = document.querySelector("#earn");
-const tbody = document.querySelector("#bod");
-const table = document.querySelector("#tab");
-
-
-
-function createSale(order)
-{
-
-     const tr = document.createElement("tr")
-     const name = document.createElement("td")
-     const price = document.createElement("td")
-     const payStatus = document.createElement("td")
-     const delStatus = document.createElement("td")
-     const span = document.createElement("span")
-
-     name.textContent = order.name;
-     price.textContent = `${order.price} EGP`;
-     payStatus.textContent = order.payStatus;
-     span.classList.add("status",order.delStatus)
-     span.textContent  = order.delStatus;
-     delStatus.appendChild(span);
-
-     tr.appendChild(name)
-     tr.appendChild(price)
-     tr.appendChild(payStatus)
-     tr.appendChild(delStatus)
-
-     tbody.appendChild(tr);
-
-}
-
-function createCustomer(customer)
-{
-
-const tr = document.createElement("tr")
-const td = document.createElement("td")
-const h4 = document.createElement("h4")
-h4.innerHTML = `${customer.name} <br> <span>${customer.country}</span>`
-td.appendChild(h4)
-tr.appendChild(td)
-table.appendChild(tr)
-
-       
-}  
-
-console.log(numofsales,earning,table,table)
-fetch("https://ecommerce-new.onrender.com/dash")
-.then(response => {
-  if (response.ok) {
-    return response.json(); // or response.text() for plain text response
-  }
-  throw new Error('Network response was not ok.');
-})
-.then(data => {
-  console.log(data)
-  numofsales.textContent = data.numofsales;
-  earning.textContent =`${data.earning} EGP` ;
-  data.customers.forEach((ele)=>
-  {
-     createCustomer(ele);
-  })
-  data.products.forEach((ele)=>
-  {
-     createSale(ele);
-  })
-
-
-})
-.catch(error => {
-  console.error('There was a problem with the fetch operation:', error);
-});
