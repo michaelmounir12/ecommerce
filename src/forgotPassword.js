f7385359ed8e4d641c7167818e9bbcb08948b634
michael <michaelmounirmnb@gmail.com>
2023-05-14 13:54:02 +0300

    newFile

--- src/forgotPassword.js
+++ src/forgotPassword.js
@@ -70,7 +70,7 @@ async function forgotPass(req,res,next)
 async function resetPass(req,res,next)
 {
     // console.log(req.body.password)
-    const token = req.params.token.trim()
+    const token = req.params.token
     console.log(req.params)
     const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
     console.log(hashedToken)
