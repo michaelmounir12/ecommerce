df1e67d654126fca255fd4c426deabd15ce7a90c
michael <michaelmounirmnb@gmail.com>
2023-05-14 14:06:36 +0300

    newFile

---
+++ src/public/rp.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("button");
+      const pass = document.querySelector("#new-password")
+      btn.addEventListener("click",()=>
+      {
+        console.log(window.location.href)
+           fetch(window.location.href, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ 
+   password:pass.value
+  })
+})
+.then()
+.catch(error => console.error(error));
+      })
